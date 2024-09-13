8eddadeecb7aba290d74e1b8a6cdc35a11a2f8a9
rajani803 <rajanigiri56@gmail.com>
2024-09-13 20:33:23 +0545

    Add files via upload

--- app.py
+++ app.py
@@ -1,40 +1,28 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 import random
-from flask import Flask, render_template, jsonify
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/move_no_button')
-def move_no_button():
-    new_x = random.randint(100, 900)
-    new_y = random.randint(100, 600)
-    return jsonify({'x': new_x, 'y': new_y})
-
-if __name__ == "__main__":
-    app.run(debug=True)
 
 myApp = tk.Tk()
 myApp.geometry("1200x800")
 myApp.configure(background='light blue')
 myApp.title("Do You Like Me?")
 
+
 mainMenu = tk.Frame(myApp, background='light blue')
 mainMenu.place(relx=0.5, rely=0.5, anchor='center')
+
 
 def move_no_button():
     new_x = random.randint(100, 900)
     new_y = random.randint(100, 600)
     btn_no.place(x=new_x, y=new_y)
 
+
 def display_gif():
     gif_path = "cat.gif"  
     img = Image.open(gif_path)
 
+   
     frames = []
     try:
         while True:
@@ -48,7 +36,7 @@ def display_gif():
         myApp.after(100, update_gif, (index + 1) % len(frames))
 
     label = tk.Label(mainMenu, bg='light blue')
-    label.grid(column=0, row=0, columnspan=2, pady=20)
+    label.grid(column=7, row=4)
     update_gif(0)
 
 def display_heart_gif():
@@ -66,7 +54,7 @@ def display_heart_gif():
         pass 
 
     heart_label = tk.Label(myApp, bg='light blue')
-    heart_label.place(relx=0.5, rely=0.5, anchor='center')  
+    heart_label.place(relx=0, rely=0, relwidth=1, relheight=1)  
 
     tk.Label(myApp, text="I know", font=('Helvetica', 24, 'bold italic'), bg='light blue', fg='dark blue').place(relx=0.5, rely=0.9, anchor='center')
 
@@ -75,22 +63,28 @@ def display_heart_gif():
         index = (index + 1) % len(heart_frames)  
         myApp.after(100, update_heart_gif, index)  
 
+    
     update_heart_gif(0)
+
 
 text = tk.Label(mainMenu, text="Do you like me?", font=('Helvetica', 24, 'bold italic'),
                 background='light blue', bd=20, fg='dark blue')  
-text.grid(column=0, row=1, pady=20)
+text.grid(column=7, row=5, pady=20)
+
 
 btn_yes = tk.Button(mainMenu, text="Yes", font=('Helvetica 14 bold italic'),
                     background='pink', activebackground='red', bd=5, height=2,
                     width=10, fg='white', relief=tk.SOLID, command=display_heart_gif)
-btn_yes.grid(column=1, row=2, padx=20)
+btn_yes.grid(column=10, row=5, padx=20)
 
-btn_no = tk.Button(mainMenu, text="No", font=('Helvetica 14 bold italic'),
+
+btn_no = tk.Button(myApp, text="No", font=('Helvetica 14 bold italic'),
                    background='pink', activebackground='red', bd=5, height=2,
                    width=10, fg='white', relief=tk.SOLID, command=move_no_button)
-btn_no.grid(column=1, row=3, padx=20)
+btn_no.place(x=500, y=400)  
+
 
 display_gif()
+
 
 myApp.mainloop()
